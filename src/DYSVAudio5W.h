d2e54c9afb4696db8047cbc5dbc7a02015143562
Manjunathan_6751 <75794733+Manjunathan1998@users.noreply.github.com>
2025-12-06 23:04:22 +0530

    Update DYSVAudio5W.h

--- src/DYSVAudio5W.h
+++ src/DYSVAudio5W.h
@@ -13,7 +13,22 @@
 
   Note: This is a beta release. Some functions may change in future versions.
   Author: Manjunathan
-  Version: 1.0.0-Beta-01
+  Date: 06-DEC-2025
+
+  License:
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see <http://www.gnu.org/licenses/>.
+  
 */
 
 

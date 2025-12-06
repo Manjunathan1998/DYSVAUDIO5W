30519528b00810f6a61fdb337c09925cf72fbb67
Manjunathan_6751 <75794733+Manjunathan1998@users.noreply.github.com>
2025-12-06 22:26:38 +0530

    Delete dy_sv_utils.h

--- dy_sv_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef DY_SV_UTILS_H
-#define DY_SV_UTILS_H
-
-#include <Arduino.h>
-int query_total_num_song(Stream &serial_comm);
-int query_current_playback_state(Stream &serial_comm);
-void control_command(Stream &serial_comm);
-#endif
